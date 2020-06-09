91676b83488629e6050994aea694a65ff99491e3
Pavel Novitskiy <novipsh@outlook.com>
2020-06-09 05:06:13 +0300

    Code formatting

--- central-car-lock-bluetooth.ino
+++ central-car-lock-bluetooth.ino
@@ -15,24 +15,29 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
     private: 
       const int ON = LOW;
       const int OFF = HIGH;
+      
       /*
-        адреси виходів, що будуть контролювати процес роботи
+        Адреси виходів, що будуть контролювати процес роботи
       */
       const int PERMISSION_RELAY = 25;
       const int POLARITY_CHANGE_RELAY_1 = 26;
       const int POLARITY_CHANGE_RELAY_2 = 27;
+      
       /*
-        оголошення затримки, яка знадобиться для паузи між змінами положень реле
+        Затримка, яка знадобиться для паузи між змінами положень реле
       */
       const int SWITCHING_DELAY = 1000;
+      
       /*
-        оголошуємо конфігураційні ідентифікатори для функціонування Bluetooth
+        Конфігураційні ідентифікатори для функціонування Bluetooth
       */
+      
       const std::string BLUETOOTH_DEVICE_NAME = "Car Lock";
       const std::string BLUETOOTH_SERVICE_UUID = "641862ac-9bdc-43d1-a2ba-2dca6d9ac892";
       const std::string BLUETOOTH_CHARACTERISTIC_UUID = "e220ff7f-5d58-4dc6-929b-367de4026d32";
+      
       /*
-        дані-тригери, на які відгукнеться контролер, щоб закрити чи 
+        Дані-тригери, на які відгукнеться контролер, щоб закрити чи 
         відкрити замок, якщо отримує ці символи за Bluetooth з'єднанням 
       */
       const std::string TURN_ON_TRIGGER = "ON";
@@ -47,8 +52,9 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
       digitalWrite(PERMISSION_RELAY, OFF);
       delay(SWITCHING_DELAY);
     }
+    
     /*
-      оголошуємо виходи на піни трьох реле, двоє з яких 
+      Оголошуємо виходи на піни трьох реле, двоє з яких 
       відповідаються за зміну полярності на замковому приводі,
       а третє реагує на подання струму та пропускає його далі, щоб 
       дозволити зреагувати цим двом реле на цю зміну і змінити свій стан
@@ -58,8 +64,9 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
       pinMode(POLARITY_CHANGE_RELAY_1, OUTPUT);
       pinMode(POLARITY_CHANGE_RELAY_2, OUTPUT);
     }
+    
     /*
-      оголошення Bluetooth Low Enegry (BLE) Server
+      Оголошення Bluetooth Low Enegry (BLE) Server
     */
     public: void initBluetoothServer() {
       BLEDevice::init(BLUETOOTH_DEVICE_NAME);
@@ -81,13 +88,14 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
       BLEAdvertising *carLockBleAd = carLockBleServer -> getAdvertising();
       carLockBleAd -> start();
     }
+    
     /*
-      оголошуємо команди, що будуть виконувати зміну полярності на релейних
+      Оголошуємо команди, що будуть виконувати зміну полярності на релейних
       пристроях та закривати чи відкривати замковий механізм відповідно
     */
     public: void carLock() {
       /*
-        змінюємо полярність реле на (-) та закриваємо замок
+        Змінюємо полярність реле на (-) та закриваємо замок
       */
       permissionRelayOff();
       digitalWrite(POLARITY_CHANGE_RELAY_1, OFF);
@@ -97,7 +105,7 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
     
     public: void carUnlock() {
       /*
-        змінюємо полярність реле на (+) та відкриваємо замок
+        Змінюємо полярність реле на (+) та відкриваємо замок
       */
       permissionRelayOff();
       digitalWrite(POLARITY_CHANGE_RELAY_1, ON);
@@ -108,9 +116,9 @@ class CarLockSystemCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *carLockBleChar) {
       std::string value = carLockBleChar -> getValue();
       /*
-         для перевірки роботи схеми перевіряємо за зміною
-         отриманного значення на  чи 1, що буде виконуватись
-         відкриття чи закриття замка
+         Для роботи замка перевіряємо дані отримані з підключеного
+         Bluetooth пристрою. Якщо вони відповідають прописаним 
+         умовам, буде виконуватись відкриття чи закриття замка
       */
       if (value == TURN_ON_TRIGGER) {
         carLock();
